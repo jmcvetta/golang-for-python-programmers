c6a66288f5c78f46e08fb7df2c509a5fa3bab486
Rahim Nathwani <rahim@encona.com>
2018-11-14 12:53:01 +0800

    Split hello.py into separate library and application packages

---
+++ source/examples/python/hello/greeter.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+import time
+
+
+def greeting():
+    '''Returns a pleasant, semi-useful greeting.'''
+    return "Hello world, the time is: " + time.ctime()
+
--- source/examples/python/hello/hello.py
+++ source/examples/python/hello/hello.py
@@ -1,15 +1,7 @@
 #!/usr/bin/env python
-#
-# Copyright (c) 2013 Jason McVetta.  This is Free Software, released under the
-# terms of the GPL v3.  See http://www.gnu.org/copyleft/gpl.html for details.
-# Resist intellectual serfdom - the ownership of ideas is akin to slavery.
 
 import time
-
-
-def greeting():
-    '''Returns a pleasant, semi-useful greeting.'''
-    return "Hello world, the time is: " + time.ctime()
+from greeter import greeting
 
 
 def main():
@@ -18,4 +10,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
